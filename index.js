8572a09eb1aba0ae103e57725a442f3902039034
JadenFicklin <jadendficklin@gmail.com>
2022-01-12 14:09:38 -0700

    Completed part 2

--- index.js
+++ index.js
@@ -22,21 +22,31 @@ axios.get("https://swapi.dev/api/people/?search=millennium");
 axios.get("https://practiceapi.devmountain.com/api");
 
 // Check if the POST request accept params, queries, and/or a body. Which one(s) and what information is it expecting to be sent?
+// post expects an object with a key of text and value type of a string
 
 // What data type does the GET request return?
+//an object
 
 // What would the URL look like for deleting the post with the id 555? (This post does not exist anymore, but the syntax is the same for existing posts, )
+axios.delete("https://practiceapi.devmountain.com/api/posts/555");
 
 // List the possible response codes from the GET request at ‘/posts/filter’
+//Request query is missing required text property.
 
 // Create a post whose text is your name, record the URL and body here:
+axios.post("https://practiceapi.devmountain.com/api/posts/", {
+  text: "Jaden",
+});
 
 // What would the URL and body object be to update the post you just made to contain your faovrite color instead of your name?
-
+axios.put("https://practiceapi.devmountain.com/api/posts/409", {
+  text: "Orange",
+});
 // What is the URL to get posts that contain the text “blue”?
-
+axios.get("https://practiceapi.devmountain.com/api/posts/?text=blue");
 // Make a request to GET all the posts. What are the content type and charset of the response? (Hint: look on the Headers)
 
 // What would cause a PUT request to return a 409 status?
+// Request was missing req.query.id or req.body.text
 
 // What happens if you try to send a query in the GET request URL? Why do you get that response?
