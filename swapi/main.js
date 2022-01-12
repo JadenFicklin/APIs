3075949999392a466ccc6e19fbf73b312024c5c6
JadenFicklin <jadendficklin@gmail.com>
2022-01-12 16:17:37 -0700

    Made pokemon compatable api

--- swapi/main.js
+++ swapi/main.js
@@ -1,20 +1,47 @@
 //get html elements
 let getButton = document.querySelector(".residents-button");
-let section = document.querySelector(".info-here");
+let card = document.querySelector(".info-here");
+let nameCard = document.querySelector(".name");
+let heightCard = document.querySelector(".height");
+let weightCard = document.querySelector(".weight");
 
 const displayName = (name) => {
   let t = document.createTextNode(name);
-  section.appendChild(t);
+  nameCard.appendChild(t);
+};
+
+const displayHeight = (height) => {
+  let t = document.createTextNode(height);
+  heightCard.appendChild(t);
+};
+
+const displayWeight = (weight) => {
+  let t = document.createTextNode(weight);
+  weightCard.appendChild(t);
 };
 
 //api
-const getDarthVader = () =>
+const dittoInfo = () =>
   axios
-    .get("https://swapi.dev/api/people/4")
+    .get("https://pokeapi.co/api/v2/pokemon/ditto")
     .then((res) => {
       displayName(res.data.name);
+      displayHeight(res.data.height);
+      displayWeight(res.data.weight);
     })
-    .catch("err");
+    .catch("error");
+
+//what will happen when button is clicked
+// function button() {
+//   getDarthVader();
+// }
 
 //function once button is clicked
-getButton.addEventListener("click", getDarthVader);
+getButton.addEventListener("click", dittoInfo);
+
+// <div class="info-here">
+// <p class="name"></p>
+// <p class="height"></p>
+// <p class="weight"></p>
+// <img class="picture" src="" />
+// </div>
